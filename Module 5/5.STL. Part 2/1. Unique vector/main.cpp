e6d14f83cb0001dddd1b497aaa0e64527b0abb3a
Shepelov <buzuluk_gb@inbox.ru>
2025-07-31 20:00:28 +0500

    add STL. Part 2

---
+++ Module 5/5.STL. Part 2/1. Unique vector/main.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+std::vector<int> unique_vector(std::vector<int> vector) {
+	std::sort(vector.begin(), vector.end());
+	vector.erase(std::unique(vector.begin(), vector.end()), vector.end());
+
+	return vector;
+}
+
+void print_vector(std::vector<int> vector) {
+	for (int i : vector) {
+		std::cout << i << " ";
+	}
+	std::cout << std::endl;
+}
+
+int main(int argc, char* argv[])
+{
+	std::vector<int> vec = { 1, 1, 2, 5, 6, 1, 2, 4 };
+
+	std::cout << "[IN]: ";
+	print_vector(vec);
+	std::cout << "[OUT]: ";
+	print_vector(unique_vector(vec));
+
+	return 0;
+}
---
+++ Module 5/5.STL. Part 2/2. Print container/main.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <vector>
+#include <set>
+#include <list>
+#include <string>
+
+template <class T>
+void print_container(T container) {
+	for (const auto& i : container) {
+		std::cout << i << " ";
+	}
+	std::cout << std::endl;
+}
+
+int main(int argc, char* argv[])
+{
+	std::set<std::string> test_set = { "one", "two", "three", "four" };
+	print_container(test_set);
+
+	std::list<std::string> test_list = { "one", "two", "three", "four" };
+	print_container(test_list);
+
+	std::vector<std::string> test_vector = { "one", "two", "three", "four" };
+	print_container(test_vector);
+
+	return 0;
+}
