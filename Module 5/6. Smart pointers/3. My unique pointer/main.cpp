f54f1eff957aa25c0f9d45fdc9140fb640334a70
Shepelov <buzuluk_gb@inbox.ru>
2025-08-04 20:59:12 +0500

    fix my unique ptr

--- Module 5/6. Smart pointers/3. My unique pointer/main.cpp
+++ Module 5/6. Smart pointers/3. My unique pointer/main.cpp
@@ -7,7 +7,7 @@ private:
     T& operator= (const T& other) = delete; //запрещаем использование оператора =
     my_unique_ptr(const my_unique_ptr&) {}  //запрещаем использование конструктор копирования
 public:
-    my_unique_ptr(T* raw_ptr) : ptr{raw_ptr} {}
+    my_unique_ptr(T* raw_ptr) : ptr{ raw_ptr } {}
 
     ~my_unique_ptr() {
         if (ptr) {
@@ -16,7 +16,15 @@ public:
     }
 
     T operator* () {
-        return *ptr;
+        if (ptr) {
+            return *ptr;
+        }
+    }
+
+    T* operator-> () {
+        if (ptr) {
+            return ptr;
+        }
     }
 
     T* release() {
@@ -32,6 +40,10 @@ int main()
     my_unique_ptr<int> my_ptr(new int(10));
     std::cout << *my_ptr << std::endl;
     int* ptr = my_ptr.release();
+    
+    //std::string str = "Hello world!"
+    my_unique_ptr<std::string> my_ptr2(new std::string("Hello wolrd!"));
+    std::cout << my_ptr2->size() << std::endl;
 
     return 0;
 }
